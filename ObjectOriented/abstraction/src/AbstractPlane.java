5453f1065a27213ac5aa67b67166ed91f0669352
auspiciouskunal <auspiciouskunal@gmail.com>
2025-05-17 20:18:05 +0530

    Abstract | Abstract using abstract keyword

---
+++ ObjectOriented/abstraction/src/AbstractPlane.java
@@ -0,0 +1,9 @@
+abstract public class AbstractPlane {
+    abstract public void fly();
+    abstract public void takeOff();
+    abstract public void landing();
+
+    public void airport(){
+        System.out.println("Plane is at airport");
+    }
+}
---
+++ ObjectOriented/abstraction/src/ChildCargoPlane.java
@@ -0,0 +1,14 @@
+public class ChildCargoPlane extends AbstractPlane{
+
+    public void fly(){
+        System.out.println("Cargo Plane Flies");
+    }
+
+    public void landing(){
+        System.out.println("Cargo Plane lands");
+    }
+
+    public void takeOff(){
+        System.out.println("Cargo Plane takeOffs");
+    }
+}
